0096d2ea221a3fa1415fb1c006fef39415347a22
DorianCBrwn <brown.3794@gmail.com>
2023-12-05 15:07:35 -0500

    update parameters

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -ex
 
-STACK_NAME=${1:-data-science-dev-env}
-REGION=${2:-ap-southeast-1}
+STACK_NAME=${1:-devOps-env}
+REGION=${2:-ap-northeast-1}
 PROFILE=${3:-default}
 
 deploy () {
 
   local CMD="aws cloudformation --region=${REGION} --profile=${PROFILE}"
-          
+
   ${CMD} deploy \
   --stack-name ${STACK_NAME} \
-  --template-file ./templates/ec2.yml \
+  --template-file ./templates/ec2-ubuntu.yml \
   --parameter-overrides $(cat config/deploy.ini) \
   --capabilities CAPABILITY_NAMED_IAM CAPABILITY_IAM CAPABILITY_AUTO_EXPAND
 }
